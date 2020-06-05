d71feb32d16f4992330ae99d1e25d76282ba08a2
alexisyanez <adanilo.yanez@gmail.com>
2020-06-05 13:33:04 -0400

    defining CycleApp

--- src/modules/application/CycleAppLayer.h
+++ src/modules/application/CycleAppLayer.h
@@ -13,8 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#ifndef MODULES_APPLICATION_PEDAPPLAYER_H_
-#define MODULES_APPLICATION_PEDAPPLAYER_H_
+#ifndef MODULES_APPLICATION_CYCLEAPPLAYER_H_
+#define MODULES_APPLICATION_CYCLEAPPLAYER_H_
 
 #include <omnetpp.h>
 #include "modules/application/ieee80211p/BaseWaveApplLayer.h"
